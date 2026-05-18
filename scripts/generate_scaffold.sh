4b2a6c19603f75ea7598ac45aa32af070c8f109b
erwan-simon <erwan.simon@devoteam.com>
2026-05-18 10:53:38 +0200

    fix(scaffold): drop personal identifiers from default scaffold values

--- scripts/generate_scaffold.sh
+++ scripts/generate_scaffold.sh
@@ -22,8 +22,8 @@ cookiecutter "${REPO_ROOT}/cookiecutter_template" \
     aws_region="${AWS_DEFAULT_REGION:-eu-west-1}" \
     terraform_backend_bucket_name="${TERRAFORM_BACKEND_BUCKET:?TERRAFORM_BACKEND_BUCKET must be set}" \
     terraform_backend_dynamodb_name="${TERRAFORM_BACKEND_DYNAMODB:?TERRAFORM_BACKEND_DYNAMODB must be set}" \
-    failure_notification_receivers="erwan.simon+datalake_tests@revolve.team" \
-    datalake_admin_principal_arns="AWSReservedSSO_AdministratorAccess_b46bf0a32c9dd401,federated-access,poc_devops_platform_ci_access_prod_gitlab_oidc_access,datazone_usr_role_3vtoy9hfgod2xi_d9p4135fusi7py" \
+    failure_notification_receivers="" \
+    datalake_admin_principal_arns="" \
     git_repository="${git_repository}" \
     skip_emr_serverless_sandbox_creation=true \
     domain_factory_source="../../../domain_factory" \
